bcde89add4a9fc0939175f511ffae668ba121531
Jerry Melton <jerryleemelton@gmail.com>
2024-10-02 12:44:34 -0700

    Add Konva.js for graphics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import { Stage, Layer, Circle } from "react-konva"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Circle
+            x={window.innerWidth / 2}
+            y={window.innerHeight / 2}
+            radius={50}
+            fill="red"
+            stroke="black"
+            shadowBlur={10}
+          />
+        </Layer>
+      </Stage>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
