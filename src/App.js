8c2fda79ddd797f54b88e2202fc8ac08665a39b5
Jerry Lee Melton <jerryleemelton@gmail.com>
2026-04-07 13:25:54 -0700

    Fine tune CRT effect

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
     <div className="App">
       <AsteroidsGame
         crtOptions={{
-          bloomIntensity: 1, // 0-1, glow around bright elements
-          bloomSpread: 3, // blur radius for bloom passes
-          scanlineIntensity: 0.28, // 0-1, darkness of scanline gaps
-          grilleIntensity: 0.15, // 0-1, visibility of RGB phosphor stripes
-          noiseIntensity: 0.03, // 0-1, analog static speckle
-          brightnessBoost: 1.2, // multiplier to compensate for darkening
+          bloomIntensity: 0.8, // 0-1, glow around bright elements
+          bloomSpread: 2.2, // blur radius for bloom passes
+          scanlineIntensity: 0.3, // 0-1, darkness of scanline gaps
+          grilleIntensity: 0.25, // 0-1, visibility of RGB phosphor stripes
+          noiseIntensity: 1, // 0-1, analog static speckle
+          brightnessBoost: 1.12, // multiplier to compensate for darkening
         }}
       />
     </div>
