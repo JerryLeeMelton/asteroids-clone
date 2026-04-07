b20c0a06cdea2d3f6ef953f0087ae7eb0125789e
Jerry Lee Melton <jerryleemelton@gmail.com>
2026-04-07 08:16:01 -0700

    Add options for CRT shader to game component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import AsteroidsGame from "./components/AsteroidsGame"
 function App() {
   return (
     <div className="App">
-      <AsteroidsGame width={800} height={600} />
+      <AsteroidsGame
+        crtOptions={{
+          bloomIntensity: 1, // 0-1, glow around bright elements
+          bloomSpread: 3, // blur radius for bloom passes
+          scanlineIntensity: 0.28, // 0-1, darkness of scanline gaps
+          grilleIntensity: 0.15, // 0-1, visibility of RGB phosphor stripes
+          noiseIntensity: 0.03, // 0-1, analog static speckle
+          brightnessBoost: 1.2, // multiplier to compensate for darkening
+        }}
+      />
     </div>
   )
 }
